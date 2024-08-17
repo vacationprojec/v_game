e44c45f4e000e1d22682f7fb5a0def0a9176038d
gogooga1 <dudrn322056@naver.com>
2024-08-17 22:01:04 +0900

    1111

--- Assets/Scripts/MonsterLogic.cs
+++ Assets/Scripts/MonsterLogic.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 public class MonsterLogic : MonoBehaviour
 {
     public float speed;
+    public float health;
+    public float maxHealth;
     public Rigidbody2D target;
+    WaitForFixedUpdate wait;
 
-    bool isLive = true;
+    bool isLive;
 
     Rigidbody2D rigid;
     SpriteRenderer spriter;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         spriter = GetComponent<SpriteRenderer>();
+        wait = new WaitForFixedUpdate();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (!isLive)
+        if (!isLive )
             return;
         Vector2 dirVec = target.position - rigid.position;
         Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
@@ -39,5 +45,44 @@ public class MonsterLogic : MonoBehaviour
     private void OnEnable()
     {
         target = GameManager.instance.player.GetComponent<Rigidbody2D>();
+        isLive = true;
+        health = maxHealth;
+    }
+
+    public void Init(SpawnData data)
+    {
+        speed = data.speed;
+        maxHealth = data.health;
+        health = data.health;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (false/*!collision.CompareTag("")*/)
+            return;
+        //health -= collison.GetComponent<>().damage;
+        StartCoroutine(KnockBack());
+
+        if(health > 0)
+        {
+
+        }
+        else
+        {
+            Dead();
+        }
+    }
+
+    IEnumerator KnockBack()
+    {
+        yield return wait;
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 dirvec = transform.position - playerPos;
+        rigid.AddForce(dirvec.normalized * 3, ForceMode2D.Impulse);
+    }
+
+    void Dead()
+    {
+        gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public Transform[] spawnPoint;
+    public SpawnData[] spawnData;
 
     float timer;
 
@@ -26,7 +27,20 @@ public class Spawner : MonoBehaviour
 
     void Spawn()
     {
-        GameObject enemy = GameManager.instance.pool.Get(0);
+        int type = Random.Range(0, spawnData.Length);
+        GameObject enemy = GameManager.instance.pool.Get(type);
         enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
+        enemy.GetComponent<MonsterLogic>().Init(spawnData[type]);
     }
+
+    
+}
+
+[System.Serializable]
+public class SpawnData
+{
+    public float spawnTime;
+    public int spriteType;
+    public int health;
+    public float speed;
 }
